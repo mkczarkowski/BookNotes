d1bf3f75b9837d6de971cfde8e40ce9191c79704
mkczarkowski <marcinczarkowski@onet.eu>
2017-02-03 22:52:16 +0100

    Fixed tax calculation, changed from retail to wholesale

--- Up&Going/phoneShop.js
+++ Up&Going/phoneShop.js
@@ -1,40 +1,37 @@
-const TAX_RATE = 0.23;
-const PHONE_PRICE = 79.99;
+const TAX_RATE = 0.08;
+const PHONE_PRICE = 99.99;
 const ACCESSORY_PRICE = 9.99;
-const SPENDING_TRESHOLD = 1000;
-let bankAccountBalance = 1300;
+const SPENDING_TRESHOLD = 200;
+let bankAccountBalance = 303.91;
 let moneySpent = 0;
 
-while (canIAffordIt(calculatePurchasePrice(), bankAccountBalance)) {
-    let fullPurchasePrice = calculatePurchasePrice();
-    console.log(`Koszt telefonu: ${formatAmount(fullPurchasePrice)}`);
-    buyPhone(fullPurchasePrice);
+while (canIAffordIt(moneySpent, bankAccountBalance)) {
+  buyPhone();
+}
+moneySpent = addTax(moneySpent);
+console.log(`Your purchase is worth ${formatAmount(moneySpent)}`);
+
+if (moneySpent > bankAccountBalance) {
+  console.log(`Whoops, you can't afford this!`);
 }
 
-function calculatePurchasePrice() {
-    let purchasePriceWithoutTax = PHONE_PRICE;
-    if (SPENDING_TRESHOLD > moneySpent) {
-        purchasePriceWithoutTax += ACCESSORY_PRICE;
-    }
-    let fullPrice = addTax(purchasePriceWithoutTax);
-    return fullPrice
+function buyPhone() {
+  moneySpent += PHONE_PRICE;
+  if (SPENDING_TRESHOLD > moneySpent) {
+    moneySpent += ACCESSORY_PRICE;
+  }
 }
 
-function buyPhone(phonePrice) {
-    bankAccountBalance -= phonePrice;
-    moneySpent += phonePrice;
-    console.log(`Wydane pieniądze: ${formatAmount(moneySpent)}`);
-}
-
-function canIAffordIt(price, bankAccountBalance) {
-    if (price < bankAccountBalance) {
-        return true;
-    } return false;
+function canIAffordIt(amount, bankAccountBalance) {
+  if (amount < bankAccountBalance) {
+    return true;
+  }
+  return false;
 }
 
 function addTax(priceWithoutTax) {
-    return priceWithoutTax  = priceWithoutTax + (priceWithoutTax * TAX_RATE);
+  return priceWithoutTax = priceWithoutTax + (priceWithoutTax * TAX_RATE);
 }
 function formatAmount(price) {
-    return `$${price.toFixed(2)}`;
+  return `$${price.toFixed(2)}`;
 }
